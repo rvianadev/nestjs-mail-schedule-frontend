65edc41dadced9d145f1b36efe2d5ee23007b22d
Rodrigo <roviol@gmail.com>
2024-01-03 10:48:06 -0300

    first commit

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,11 @@
+export function Nav() {
+  return (
+    <nav className="w-full h-20 bg-gray-50 border-b border-gray-200">
+      <div className="w-full max-w-7xl border-2 m-auto">
+        <p>
+          Schedule<span>Mail</span>
+        </p>
+      </div>
+    </nav>
+  );
+}
