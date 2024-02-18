eb6a725f9e74cbd8e3e261b2ca710f287bd18fe8
rvianadev <roviol@gmail.com>
2024-02-18 14:53:02 -0300

    react hook form

--- src/pages/WriteNowPage.tsx
+++ src/pages/WriteNowPage.tsx
@@ -1,21 +1,28 @@
+import { useForm } from 'react-hook-form';
 import { Button, DateTimePicker, Nav, RichInput } from 'components';
 
 export function WriteNowPage() {
+  const { register, handleSubmit } = useForm();
+
+  function onSubmit(values: any) {
+    console.log(values);
+  }
+
   return (
     <>
       <Nav />
       <div className="container">
         <h1>Escrever Agora</h1>
 
-        <form className="form">
+        <form className="form" onSubmit={handleSubmit(onSubmit)}>
           <label htmlFor="destinationName">Nome completo</label>
-          <input type="text" />
+          <input {...register('destinationName')} type="text" />
           <label htmlFor="destinationAddress">E-mail</label>
-          <input type="email" />
+          <input {...register('destinationAddress')} type="email" />
           <label htmlFor="dueDate">Data</label>
           <DateTimePicker />
           <label htmlFor="subject">Assunto</label>
-          <input type="text" />
+          <input {...register('subject')} type="text" />
           <label htmlFor="body">Mensagem</label>
           <RichInput />
           <Button type="submit" variant="primary">
